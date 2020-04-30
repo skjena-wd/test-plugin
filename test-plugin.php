18d98a9cc52cb6e3a5c1eca6fcfaeeca870d0ef7
skjena-wd <skjena2004j@gmail.com>
2020-04-30 12:29:39 +0000

    plugin characteristics added

--- test-plugin.php
+++ test-plugin.php
@@ -1,5 +1,17 @@
 <?php
+/* 
+* Plugin Name:  Test Plugin
+* Plugin URI:  http://accenture.com
+* Description:  This is a plugin for testing Custom Post Type "Devices"
+* Author:  Accenture
+* Version: 1.0.0
+* Author URI:  http://accenture.com
+* 
+* 
+* 
+*/
 
+defined('ABSPATH') or die('You cannot access this file!');
 
 
 
