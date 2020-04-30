d9dd168e92af6f6f4b81b6bb1c7493a47a36c5d7
skjena-wd <skjena2004j@gmail.com>
2020-04-30 12:43:46 +0000

    activation and deactivation function added

--- test-plugin.php
+++ test-plugin.php
@@ -6,43 +6,43 @@
 * Author:  Accenture
 * Version: 1.0.0
 * Author URI:  http://accenture.com
-* 
-* 
-* 
 */
 
 defined('ABSPATH') or die('You cannot access this file!');
 
-class TestPlugin
-{
-	function __construct()
-	{
+class TestPlugin {
+	function __construct(){
 		add_action('init', array($this, 'custom_post_type'));
 	}
 	
-	function custom_post_type()
-	{
+	function custom_post_type(){
 		register_post_type('device', ['public'=>'true', 'label'=>'Devices']);
 	}
 	
-	function activate()
-	{
+	function activate(){
 		//calling function custom_post_type
 		$this->custom_post_type();  
 		//Flushing rewrite rules
 		flush_rewrite_rules();
+	}	
+	
+	function deactivate(){
+		flush_rewrite_rules();
 	}
 	
-	
+	function uninstall(){
+		//Delete CPT
+		//Delete CPT data from database
+	}
 }
 
 
-
-if(class_exists('TestPlugin'))
-{
+if(class_exists('TestPlugin')){
 	$testPlugin = new TestPlugin();
 }
 
+register_activation_hook(__FILE__, array($testPlugin, 'activate'));
 
+register_deactivation_hook(__FILE__, array($testPlugin, 'deactivate'));
 
 ?>
