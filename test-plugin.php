52d2eca194452d659134765fe60906980a08b8cc
skjena-wd <skjena2004j@gmail.com>
2020-04-30 14:37:41 +0000

    dependency code added

--- test-plugin.php
+++ test-plugin.php
@@ -10,7 +10,18 @@
 
 defined('ABSPATH') or die('You cannot access this file!');
 
+if(is_multisite()) {
+    add_action('muplugins_loaded', function (){
+        TestPlugin::init();;
+    }, 55);
+} else {
+    TestPlugin::init();
+}
+
 class TestPlugin {
+    const NAME = 'Test Plugin';
+    const OPTION_INSTALLER_VERSION = 'test-installer-version';	
+	
 	function __construct(){
 		add_action('init', array($this, 'custom_post_type'));
 	}
