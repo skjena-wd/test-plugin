7c0c8889615a49aa1eb3f981f2edf5a053ac213f
skjena <skjena2004j@gmail.com>
2020-05-04 11:25:49 +0000

    initialization and register added to test-plugin

--- test-plugin.php
+++ test-plugin.php
@@ -10,18 +10,40 @@
 
 defined('ABSPATH') or die('You cannot access this file!');
 
-// if(is_multisite()) {
-//     add_action('muplugins_loaded', function (){
-//         TestPlugin::init();;
-//     }, 55);
-// } else {
-//     TestPlugin::init();
-// }
+if(is_multisite()) {
+    add_action('muplugins_loaded', function (){
+        TestPlugin::init();;
+    }, 55);
+} else {
+    TestPlugin::init();
+}
 
 class TestPlugin {
+
     const NAME = 'Test Plugin';
-    const OPTION_INSTALLER_VERSION = 'test-installer-version';	
+	const OPTION_INSTALLER_VERSION = 'test-installer-version';	
 	
+
+	/**
+	 * init plugin
+	 */
+	public static function init() {
+	// Check if Avs Web CMS is loaded
+	if(class_exists('Avs_Web_CMS') && Avs_Web_CMS::$initialized) {
+
+			// Register installer
+			AVS_WC_Extra_Installer::getInstance()->addPlugin(
+				self::OPTION_INSTALLER_VERSION,
+				__(self::NAME, UIBuilder_Naming::getSlug()),
+				plugin_dir_path(__FILE__) . 'upgrade'
+			);
+
+		}
+	}
+		
+
+	
+
 	function __construct(){
 		add_action('init', array($this, 'custom_post_type'));
 	}
