552f40e2cba7770944210a5806817a18579a4e76
skjena <skjena2004j@gmail.com>
2020-05-04 09:35:59 +0000

    comment few lines in test-plugin.php

--- test-plugin.php
+++ test-plugin.php
@@ -10,17 +10,17 @@
 
 defined('ABSPATH') or die('You cannot access this file!');
 
-if(is_multisite()) {
-    add_action('muplugins_loaded', function (){
-        TestPlugin::init();;
-    }, 55);
-} else {
-    TestPlugin::init();
-}
+// if(is_multisite()) {
+//     add_action('muplugins_loaded', function (){
+//         TestPlugin::init();;
+//     }, 55);
+// } else {
+//     TestPlugin::init();
+// }
 
 class TestPlugin {
-    const NAME = 'Test Plugin';
-    const OPTION_INSTALLER_VERSION = 'test-installer-version';	
+    // const NAME = 'Test Plugin';
+    // const OPTION_INSTALLER_VERSION = 'test-installer-version';	
 	
 	function __construct(){
 		add_action('init', array($this, 'custom_post_type'));
