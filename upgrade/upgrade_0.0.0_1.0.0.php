7bb2ad42c76e4f5de43033f9a70eb1f403f9a84d
skjena <skjena2004j@gmail.com>
2020-05-05 07:10:46 +0000

    error fixed

---
+++ upgrade/upgrade_0.0.0_1.0.0.php
@@ -0,0 +1,16 @@
+<?php
+if(!defined('TEST_UPG_100')) {
+
+    define('TEST_UPG_100', 'TEST_UPG_100');
+
+    add_action('avs_upgrade_version_'. TestPlugin::OPTION_INSTALLER_VERSION, function ($old_version, $latest_version, $plugin_key, $plugin_data) {
+
+        if(version_compare($old_version, '0.0.0') == 0){
+            
+        }
+
+    }, AVS_WC_Extra_Installer::UPGRADE_PRIORITY, 4); // Register hook always with priority 1 (order based on version number)
+
+}
+
+
